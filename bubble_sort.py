dd9a2a8b7feaeb337b187afb9f50962b0d15cabd
Shoaib Ali <56737996+shoaib-intro@users.noreply.github.com>
2019-12-02 02:34:14 +0100

    Bubble Sort

---
+++ bubble_sort.py
@@ -0,0 +1,23 @@
+'''   Bubble Sort  '''
+
+def sort(lst):
+    n=len(lst)
+    swap=False
+    for i in range(n):
+        swap=False
+        for j in range(n-i-1):
+            if(lst[j]> lst[j+1]):
+                lst[j],lst[j+1]=lst[j+1],lst[j] 
+                swap=True
+
+'  list for sorting  '
+list=[]
+n=int(input('Enter length of collection elements!'))
+for i in range(n):
+    ele=int(input('Enter '+str(i)+' Element!'))
+    list.append(ele)
+sort(list)
+print('Sorted elements are:')
+for j in range(n):
+    print(str(list[j]))
+    
